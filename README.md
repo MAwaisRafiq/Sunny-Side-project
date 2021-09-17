a58278bc01d1456916afb10719166a1c4bae29fb
MAwaisRafiq <61314054+MAwaisRafiq@users.noreply.github.com>
2021-09-17 16:49:33 +0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,55 @@
+# Frontend Mentor - Sunnyside agency landing page solution
+
+This is a solution to the [Sunnyside agency landing page challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/sunnyside-agency-landing-page-7yVs3B6ef). Frontend Mentor challenges help you improve your coding skills by building realistic projects.
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [Useful resources](#useful-resources)
+- [Author](#author)
+
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- View the optimal layout for the site depending on their device's screen size
+- See hover states for all interactive elements on the page
+
+### Screenshot
+
+![](./Screenshots/Screenshot1.png)
+
+![](./Screenshots/Screenshot2.png)
+
+
+
+### Links
+
+- Solution URL: https://github.com/MAwaisRafiq/PROJECT
+- Live Site URL: https://mawaisrafiq.github.io/PROJECT/
+
+## My process
+
+### Built with
+
+- SCSS
+- HTML
+= javascript
+
+### Useful resources
+
+- vscode
+- firefox
+
+## Author
+- Frontend Mentor - [@MAwaisRafiq](https://www.frontendmentor.io/profile/MAwaisRafiq)
+
+
---
+++ dist/scripts/menu.js
@@ -0,0 +1,50 @@
+const hamburger = document.getElementById("hamburger");
+const overlay = document.getElementById("overlay");
+const navigation = document.getElementById("navigation");
+const body = document.getElementById("body");
+
+var open = false;
+
+Popup_Mobile_Menu(open);
+
+hamburger.addEventListener("click",x =>
+{
+    open = !open;
+    Popup_Mobile_Menu(open);
+})
+
+
+function Popup_Mobile_Menu(state){
+    if(state === false)
+    {
+        if(body.classList.contains("overflow__hidden"))
+        {
+            body.classList.remove("overflow__hidden");
+        }
+        if (overlay.classList.contains("overlay"))
+        {
+            overlay.classList.remove("overlay");
+        }
+        if(navigation.classList.contains("mobile__menu"))
+        {
+            navigation.classList.remove("mobile__menu");
+            navigation.classList.add("mobile__toggle");
+        }
+
+    }else
+    {
+        if(!body.classList.contains("overflow__hidden"))
+        {
+            body.classList.add("overflow__hidden");
+        }
+        if (!overlay.classList.contains("overlay"))
+        {
+            overlay.classList.add("overlay");
+        }
+        if(navigation.classList.contains("mobile__toggle"))
+        {
+            navigation.classList.add("mobile__menu");
+            navigation.classList.remove("mobile__toggle");
+        }
+    }
+}
